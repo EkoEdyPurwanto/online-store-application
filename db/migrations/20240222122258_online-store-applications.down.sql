956bbe3a77ee05dd423a9f1aa3ec8e6add0dd72c
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-02-22 21:25:00 +0700

    [FEAT] declare db schema up/down for online-store-applications

---
+++ db/migrations/20240222122258_online-store-applications.down.sql
@@ -0,0 +1,9 @@
+DROP TYPE IF EXISTS users_status_enum;
+DROP TYPE IF EXISTS users_role_enum;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS wallets;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS carts;
+DROP TABLE IF EXISTS transaction;
---
+++ db/migrations/20240222122258_online-store-applications.up.sql
@@ -0,0 +1,71 @@
+CREATE TYPE users_status_enum AS ENUM ('active', 'inactive', 'blocked', 'other');
+CREATE TYPE users_role_enum AS ENUM ('admin', 'merchant', 'customers');
+
+CREATE TABLE IF NOT EXISTS users
+(
+    -- column
+    id            UUID PRIMARY KEY NOT NULL,
+    username      VARCHAR(25) UNIQUE,
+    password      VARCHAR(100),
+    email         VARCHAR(50) UNIQUE,
+    phone_number  VARCHAR(15) UNIQUE,
+    users_status  users_account_enum NOT NULL DEFAULT 'active',
+    role          users_role_enum NOT NULL,
+    created_at    TIMESTAMPTZ DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ
+    );
+
+CREATE TABLE IF NOT EXISTS wallets
+(
+    -- column
+    id            UUID PRIMARY KEY NOT NULL,
+    user_id       UUID REFERENCES users (id),
+    rekening_user VARCHAR(100) NOT NULL,
+    balance       BIGINT,
+    created_at    TIMESTAMPTZ DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ
+    );
+
+CREATE TABLE IF NOT EXISTS products
+(
+    -- column
+    id            UUID PRIMARY KEY NOT NULL,
+    user_id       UUID REFERENCES users (id),
+    product_name  VARCHAR(100) NOT NULL,
+    price         BIGINT NOT NULL,
+    stock         BIGINT NOT NULL,
+    category_id   UUID REFERENCES categories (id),
+    created_at    TIMESTAMPTZ DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS categories
+(
+    -- column
+    id            UUID PRIMARY KEY NOT NULL,
+    type          VARCHAR(100) NOT NULL,
+    created_at    TIMESTAMPTZ DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS carts
+(
+    -- column
+    id            UUID PRIMARY KEY NOT NULL,
+    user_id       UUID REFERENCES users (id),
+    product_id    UUID REFERENCES products (id),
+    quantity      BIGINT NOT NULL,
+    created_at    TIMESTAMPTZ DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS transaction
+(
+    -- column
+    id            UUID PRIMARY KEY NOT NULL,
+    user_id       UUID REFERENCES users (id),
+    product_id    UUID REFERENCES products (id),
+    quantity      BIGINT NOT NULL,
+    total_price   BIGINT NOT NULL,
+    date          DATE
+);
