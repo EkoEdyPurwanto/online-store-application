0ae1075a7b9386ca29a5f3665ff65c4ca78c8c6f
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-02-25 01:07:11 +0700

    [STYLE] parameter naming

--- delivery/controller/users.go
+++ delivery/controller/users.go
@@ -24,21 +24,21 @@ func NewUsersController(usersUC usecase.UsersUseCase, walletUC usecase.WalletsUs
 	}
 }
 
-func (ua *UsersController) AuthRoute() {
-	rg := ua.engine.Group("/api/v1")
+func (u *UsersController) AuthRoute() {
+	rg := u.engine.Group("/api/v1")
 
-	rg.POST("/auth/register", ua.registerHandler)
-	rg.POST("/auth/login", ua.loginHandler)
+	rg.POST("/auth/register", u.registerHandler)
+	rg.POST("/auth/login", u.loginHandler)
 }
 
-func (ua *UsersController) registerHandler(c echo.Context) error {
+func (u *UsersController) registerHandler(ctx echo.Context) error {
 	var payload req.RegisterRequest
-	err := helper.ReadFromRequestBody(c, &payload)
+	err := helper.ReadFromRequestBody(ctx, &payload)
 	if err != nil {
 		return err
 	}
 
-	err = ua.usersUC.Register(payload)
+	err = u.usersUC.Register(payload)
 	if err != nil {
 		return err
 	}
@@ -49,29 +49,29 @@ func (ua *UsersController) registerHandler(c echo.Context) error {
 		Data:    nil,
 	}
 
-	return helper.WriteToResponseBody(c, response)
+	return helper.WriteToResponseBody(ctx, response)
 }
 
-func (ua *UsersController) loginHandler(c echo.Context) error {
+func (u *UsersController) loginHandler(ctx echo.Context) error {
 	var payload req.LoginRequest
-	if err := c.Bind(&payload); err != nil {
-		return c.JSON(http.StatusBadRequest, resp.ApiResponse{
+	if err := ctx.Bind(&payload); err != nil {
+		return ctx.JSON(http.StatusBadRequest, resp.ApiResponse{
 			Status:  http.StatusBadRequest,
 			Message: "Bad Request",
 			Data:    nil,
 		})
 	}
 
-	token, err := ua.usersUC.Login(payload)
+	token, err := u.usersUC.Login(payload)
 	if err != nil {
-		return c.JSON(http.StatusUnauthorized, resp.ApiResponse{
+		return ctx.JSON(http.StatusUnauthorized, resp.ApiResponse{
 			Status:  http.StatusUnauthorized,
 			Message: "Unauthorized",
 			Data:    err.Error(),
 		})
 	}
 
-	return c.JSON(http.StatusOK, resp.ApiResponse{
+	return ctx.JSON(http.StatusOK, resp.ApiResponse{
 		Status:  http.StatusOK,
 		Message: "Successfully login",
 		Data:    token,
