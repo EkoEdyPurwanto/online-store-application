2bfa7de8a7744bcd30ddaa319a0d06e9710343e8
EkoEdyPurwanto <ekoedypurwantooke@gmail.com>
2024-02-24 23:47:01 +0700

    [FIX] change the position of everything so that it can be migrated down

--- db/migrations/20240222122258_online-store-applications.down.sql
+++ db/migrations/20240222122258_online-store-applications.down.sql
@@ -1,9 +1,11 @@
-DROP TYPE IF EXISTS users_status_enum;
-DROP TYPE IF EXISTS users_role_enum;
-
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS wallets;
+-- Drop tables
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS carts;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS carts;
-DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS wallets;
+DROP TABLE IF EXISTS users;
+
+-- Drop ENUM types
+DROP TYPE IF EXISTS users_status_enum;
+DROP TYPE IF EXISTS users_role_enum;
